dc5125b54fcaa367e6d85e9237b24b98d5ad995d
Seantheman01 <sean.v.g@outlook.com>
2023-02-13 09:52:34 +0100

    committed

--- extra/inloggen.js
+++ extra/inloggen.js
@@ -1,3 +1,10 @@
+let naam_ingelogd = "";
+
+
 function handleLogin(){
+    let naam = document.getElementById("inputNaam").value;
     console.log("U hebt op de knop geklikt!");
+    if (naam_ingelogd === ""){
+        console.log("U bent ingelogd: " + naam);
+    }
 }
