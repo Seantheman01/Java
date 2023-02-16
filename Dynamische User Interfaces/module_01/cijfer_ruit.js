678b51a005ef7559041e7c227831d7702c216964
Seantheman01 <sean.v.g@outlook.com>
2023-02-16 10:08:27 +0100

    committed

--- Dynamische User Interfaces/module_01/cijfer_ruit.js
+++ Dynamische User Interfaces/module_01/cijfer_ruit.js
@@ -1,12 +1,24 @@
-let getal = parseInt(input("Typ een getal in: "))
+let getal = parseInt(prompt("Typ een getal in: "))
+let ruit = "";
 
 for (let x = 1; x <= getal; x++) {
     for (let y = 1; y < getal - x + 1; y++) {
-        string += "";
+        ruit += "";
     }
 
     for (let z = 1; z <=2 * x - 1; z++) {
-        string += z;
+        ruit += z;
     }
-    string += "\n";
+    ruit += "\n";
 }
+for (let x = 1; x <= getal - 1; x++) {
+    for (let y = 1; y < x + 1; y++) {
+        ruit += "";
+    }
+    for (let z = 1; z <= 2 * (getal - x) - 1; z++) {
+        ruit += z;
+    }
+    ruit += "\n";
+}
+
+document.getElementById("ruit").innerHTML = ruit;
