93ecaad021d7d4add0403938ec319f24eb013b60
Seantheman01 <sean.v.g@outlook.com>
2023-02-20 09:33:51 +0100

    committed

--- Dynamische User Interfaces/module_01/cijfer_ruit.js
+++ Dynamische User Interfaces/module_01/cijfer_ruit.js
@@ -2,10 +2,10 @@ let getal = parseInt(prompt("Typ een getal in: "))
 let ruit = "";
 
 for (let x=1; x == getal; x++) {
-    for (let y=1; y == getal - x; y++) {
+    for (let y=1; y == getal - x; y++){
         ruit += "-";
     }
-    for (let z = 1; z <= 2 * (getal - x + 1) - 1; z++) {
+    for (let z = 1; z <= 2 * (getal - x + 1) - 1; z++){
         string += z;
     }
     string += "\n";
--- extra/inloggen.js
+++ extra/inloggen.js
@@ -1,14 +1,16 @@
-let naam_ingelogd = "";
+let loggedIn = "";
 
 function handleLogin(){
     let naam = document.getElementById("inputNaam").value;
     console.log("U hebt op de knop geklikt!");
-    if (naam_ingelogd === ""){
-        naam_ingelogd = naam;
-        console.log("U bent ingelogd: " + naam);
-        document.getElementById("inputNaam").value = "";
-        document.getElementById('melding').innerText = 'Gelukt!';
+    if (naam in loggedIn){
+        console.log("U zit al in de lijst.");
     }
+
+    console.log("U bent ingelogd: " + naam);
+    document.getElementById("inputNaam").value = "";
+    document.getElementById('melding').innerText = 'Gelukt!';
+
 }
 
 document.getElementById("inlogButton").onclick = handleLogin;
