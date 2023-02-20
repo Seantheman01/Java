0da4638e73d361569a5986dc855379d17f4687b7
Seantheman01 <sean.v.g@outlook.com>
2023-02-20 09:54:06 +0100

    committed

--- extra/inloggen.js
+++ extra/inloggen.js
@@ -1,16 +1,18 @@
 let loggedIn = ["Sean"];
 
 function handleLogin(){
+    console.log(loggedIn)
     let naam = document.getElementById("inputNaam").value;
     console.log("U hebt op de knop geklikt!");
     if (loggedIn.includes(naam)){
         loggedIn.splice(loggedIn.indexOf(naam), 1)
-        console.log("U zit al in de lijst: " + naam);
+        console.log("U bent uitgelogd: " + naam);
     }
 
-    console.log("U bent ingelogd: " + naam);
-    document.getElementById("inputNaam").value = "";
-    document.getElementById('melding').innerText = 'Gelukt!';
+    console.log(loggedIn)
+    // console.log("U bent ingelogd: " + naam);
+    // document.getElementById("inputNaam").value = "";
+    // document.getElementById('melding').innerText = 'Gelukt!';
 }
 
 document.getElementById("inlogButton").onclick = handleLogin;
