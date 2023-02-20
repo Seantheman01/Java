e6434ff2c5feb8ddc64c989993a2bb0383869bf0
Seantheman01 <sean.v.g@outlook.com>
2023-02-20 10:21:51 +0100

    committed

--- Dynamische User Interfaces/module_01/cijfer_ruit.js
+++ Dynamische User Interfaces/module_01/cijfer_ruit.js
@@ -1,15 +1,3 @@
-let getal = parseInt(prompt("Typ een getal in: "))
-let ruit = "";
+console.log("")
+mijn_string = "1"
 
-for (let x=1; x == getal; x++) {
-    for (let y=1; y == getal - x; y++){
-        ruit += "-";
-    }
-    for (let z = 1; z <= 2 * (getal - x + 1) - 1; z++){
-        string += z;
-    }
-    string += "\n";
-}
-
-
-console.log(ruit);
