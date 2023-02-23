192e83b443cc622725111c9068aff27d4a497148
Seantheman01 <sean.v.g@outlook.com>
2023-02-23 09:55:18 +0100

    committed

--- extra/zookeeper.js
+++ extra/zookeeper.js
@@ -1,7 +1,9 @@
 // opdracht 1:
-let poten = prompt("Hoeveel poten zijn er?");
+let giraffen = prompt("Hoeveel giraffen zijn er?");
+let struisvogels = prompt("Hoeveel struisvogels zijn er?");
+let zebras = prompt("Hoeveel zebra's zijn er?");
 
 function mijn_functie(giraffen, struisvogel, zebras) {
     return (4,2,4);
 }
-document.getElementById('poten') = mijn_functie
+document.getElementById('poten') = mijn_functie;
