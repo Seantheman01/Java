430e69a5151d1bff91c13a6e9dfd64ec66e8ddf5
Seantheman01 <sean.v.g@outlook.com>
2023-03-09 11:54:21 +0100

    committed

---
+++ Dynamische User Interfaces/module_01/cijfer_ruit.js
@@ -0,0 +1,13 @@
+let vraag = parseInt(prompt("Voer een nummer in: "))
+let string = "";
+
+for (let x=1; x<=vraag; x++){
+    for (let y = 1; y <= x; y++){
+        string += y.toString() + "-"
+    }
+    string = string.slice(0,-1);
+    string += "<br>"
+
+    document.getElementById("ruit").innerHTML += string;
+    string = ""
+}
