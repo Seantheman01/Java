654b9273969f6a51b24f45a3af9a95a8ee174ed0
Seantheman01 <sean.v.g@outlook.com>
2023-03-16 10:11:20 +0100

    committed

--- Dynamische User Interfaces/module_01/horeca_app.js
+++ Dynamische User Interfaces/module_01/horeca_app.js
@@ -1,20 +1,29 @@
 bestel_lijst = [];
+let kladblaadje = new Object();
 const bestelling = {
-    'fris': 1.99,
+    'fris': 0.99,
     'bier': 1.99,
-    'wijn': 1.99
+    'wijn': 2.99
 };
 
 while(true){
 let drankjes = prompt("Wat wilt u bestellen? Kies uit fris, bier of wijn (Typ 'stop' als u wilt stoppen): ");
-    if (drankjes != 'fris' || drankjes != 'bier' || drankjes != 'wijn' || drankjes != 'stop'){
-        alert("Kies uit: fris, bier of wijn, alstublieft")
+
+    if (drankjes == 'fris' || drankjes == 'bier' || drankjes == 'wijn'){
+        let aantal = parseInt(prompt("Hoeveel " + drankjes + " wilt u hebben?"))
+
+        kladblaadje['drankjes'] = drankjes
+        kladblaadje['aantal'] = aantal
+        kladblaadje['prijs'] = bestelling
+        console.log(kladblaadje)
     }
+
     else if (drankjes == 'stop'){
         break
+
     }
-
-let aantal = parseInt(prompt("Hoeveel " + drankjes + " wilt u hebben?"))
-
-// let nieuw = new Object();
+    else {
+        alert("Kies uit: fris, bier of wijn, alstublieft.")
+    }
 }
+alert("Dank u wel voor uw bestelling!")
