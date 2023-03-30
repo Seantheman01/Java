d7bb2160628f6854bf026be2a14cf883fdd3ca38
Seantheman01 <sean.v.g@outlook.com>
2023-03-30 12:55:34 +0200

    committed

--- extra/challenges/knop_challenge.js
+++ extra/challenges/knop_challenge.js
@@ -1,6 +1,8 @@
 var knop = document.createElement('button');
 knop.innerText= 'Klik!';
 document.body.appendChild(knop);
-var heading = document.getElementById('naam');
-heading.innerHTML = 'Welkom Sean';
-knop.onclick = heading;
+function verander_h1(){
+    var heading = document.getElementById('naam');
+    heading.innerHTML = 'Welkom Sean';
+}
+knop.onclick = verander_h1;
