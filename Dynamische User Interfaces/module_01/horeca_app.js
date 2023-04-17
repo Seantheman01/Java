ae346c237eb261a65b5e87bc57eab79d854068e9
Seantheman01 <sean.v.g@outlook.com>
2023-04-17 10:16:17 +0200

    committed

--- Dynamische User Interfaces/module_01/horeca_app.js
+++ Dynamische User Interfaces/module_01/horeca_app.js
@@ -14,10 +14,10 @@ while (true) {
             bonnetje[bestelling] = aantal;
         }else {
             bonnetje[bestelling] += aantal;
-        }
+        };
         if(isNaN(aantal)) {
             alert("Kies een aantal, alstublieft.");
-        }
+        };
     }
 
     else if (bestelling == 'stop') {
@@ -25,7 +25,7 @@ while (true) {
     }
     else {
         alert("Kies uit fris, bier of wijn, alstublieft.");
-    }
+    };
 };
 alert("Dank u wel voor uw bestelling!");
 
