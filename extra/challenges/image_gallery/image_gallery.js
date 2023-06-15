48578b89d8ea984a6c91bfff0dacdb6141e250b5
Seantheman01 <sean.v.g@outlook.com>
2023-06-15 13:59:55 +0200

    committed

--- extra/challenges/image_gallery/image_gallery.js
+++ extra/challenges/image_gallery/image_gallery.js
@@ -1,17 +1,18 @@
-plaatjes_lijst = ['fotos/leeuw.jpg', 'fotos/zebra.jpg', 'fotos/giraf.jpg']
+const plaatjes_lijst = ['fotos/leeuw.jpg', 'fotos/zebra.jpg', 'fotos/giraf.jpg']
+let getal = 0;
 
-function terug_knop() { 
-    
-}
-function vooruit_knop() {
-    
+function verder_knop() {
+    getal++;
+    if (getal == 3) {
+        getal = 0;
+    }
+    plaatje.src = plaatjes_lijst[getal];
 }
 
-plaatje.src = plaatjes_lijst[0];
-
-if (onclick == 'terug') {
-   
-}
-else if (onclick == 'verder') {
-    
+function terug_knop() {
+    getal--;
+    if (getal == -1) {
+        getal = plaatjes_lijst.length - 1;
+    }
+    plaatje.src = plaatjes_lijst[getal];
 }
