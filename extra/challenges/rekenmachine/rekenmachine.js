fc9ab561b5720b3443d3dccb6a4015684a2c5e88
Seantheman01 <sean.v.g@outlook.com>
2023-06-22 11:54:41 +0200

    committed

--- extra/challenges/rekenmachine/rekenmachine.js
+++ extra/challenges/rekenmachine/rekenmachine.js
@@ -1,40 +1,9 @@
-function knop0() {
-    var x = document.getElementById("knop").value = "0";
+function knop(e) {
+    var x = document.getElementById('knop').innerHTML;
     alert("Je hebt " + x + " aangeklikt!");
+    console.log(e)
 }
-function knop1() {
-    var x = document.getElementById("knop").value = "1";
-    alert("Je hebt " + x + " aangeklikt!");
-}
-function knop2() {
-    var x = document.getElementById("knop").value = "2";
-    alert("Je hebt " + x + " aangeklikt!");
-}
-function knop3() {
-    var x = document.getElementById("knop").value = "3";
-    alert("Je hebt " + x + " aangeklikt!");
-}
-function knop4() {
-    var x = document.getElementById("knop").value = "4";
-    alert("Je hebt " + x + " aangeklikt!");
-}
-function knop5() {
-    var x = document.getElementById("knop").value = "5";
-    alert("Je hebt " + x + " aangeklikt!");
-}
-function knop6() {
-    var x = document.getElementById("knop").value = "6";
-    alert("Je hebt " + x + " aangeklikt!");
-}
-function knop7() {
-    var x = document.getElementById("knop").value = "7";
-    alert("Je hebt " + x + " aangeklikt!");
-}
-function knop8() {
-    var x = document.getElementById("knop").value = "8";
-    alert("Je hebt " + x + " aangeklikt!");
-}
-function knop9() {
-    var x = document.getElementById("knop").value = "9";
-    alert("Je hebt " + x + " aangeklikt!");
+
+function plus() {
+    
 }
