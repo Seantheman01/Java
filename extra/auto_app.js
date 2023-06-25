3a218728822c69f783130ce57e74b3de5744966f
Seantheman01 <sean.v.g@outlook.com>
2023-06-25 21:49:15 +0200

    committed

--- extra/auto_app.js
+++ extra/auto_app.js
@@ -1,6 +1,6 @@
 // Auto kost standaard 10.000 euro
 // Vragen om deuren, 2 of 4. 4 deuren is 1000 euro extra
-// kleur vragen. Wit is standaard. Rood en blauw kosten 500 euro extra. Alle andere kleuren 1500 euro
+// Kleur vragen. Wit is standaard. Rood en blauw kosten 500 euro extra. Alle andere kleuren 1500 euro
 // Alert welke auto en prijs
 
 auto_lijst = {
