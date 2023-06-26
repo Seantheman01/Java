12023877141e21f2bfafe6593ac8b0841ffbcc70
Seantheman01 <sean.v.g@outlook.com>
2023-06-26 11:20:04 +0200

    committed

---
+++ Dynamische User Interfaces/module_02/buttons/buttons.js
@@ -0,0 +1,9 @@
+function knop1() {
+    document.getElementById("button").innerHTML = "Je hebt button1 aangeklikt!";
+}
+function knop2() {
+    document.getElementById("button").innerHTML = "Je hebt button2 aangeklikt!";
+}
+function knop3() {
+    document.getElementById("button").innerHTML = "Je hebt button3 aangeklikt!";
+}
