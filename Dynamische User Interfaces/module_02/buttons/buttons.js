82864b4f4c4deb6c8974790f0732a31bbafea898
Seantheman01 <sean.v.g@outlook.com>
2023-06-26 14:13:30 +0200

    committed

--- Dynamische User Interfaces/module_02/buttons/buttons.js
+++ Dynamische User Interfaces/module_02/buttons/buttons.js
@@ -1,5 +1,5 @@
-const plaatjes_lijst = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', 'bg0.jpg', 'bg1.jpg', 'bg2.jp', 'bg3.jpg']
+// const plaatjes_lijst = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', 'bg0.jpg', 'bg1.jpg', 'bg2.jp', 'bg3.jpg']
 
 function knop1() {
-    
+    plaatje.src = "images/1.jpg"
 }
