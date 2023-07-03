c80eead8ad1fc0b0627828e2421e36f7e3770005
Seantheman01 <sean.v.g@outlook.com>
2023-07-03 10:20:53 +0200

    committed

--- Dynamische User Interfaces/module_02/buttons/buttons.js
+++ Dynamische User Interfaces/module_02/buttons/buttons.js
@@ -19,9 +19,11 @@ const kleine_plaatjes = ['0.jpg', '1.jpg', '2.jpg', '3.jpg'];
 function groot_getal() {
     var afbeelding = document.createElement("img");
     afbeelding.src = "images/" + grote_plaatjes[0];
+    console.log(afbeelding);
 }
 
 function klein_getal() {
     let plaatje = document.createElement("img");
     plaatje.src = "images/" + kleine_plaatjes[0];
+    console.log(afbeelding);
 }
