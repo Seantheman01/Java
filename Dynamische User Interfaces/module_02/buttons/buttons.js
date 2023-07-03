b385d84f0981758bca0c9077187d0e0f8c67a2ef
Seantheman01 <sean.v.g@outlook.com>
2023-07-03 13:50:27 +0200

    committed

--- Dynamische User Interfaces/module_02/buttons/buttons.js
+++ Dynamische User Interfaces/module_02/buttons/buttons.js
@@ -1,24 +1,17 @@
 const grote_plaatjes = ['bg0.jpg', 'bg1.jpg', 'bg2.jp', 'bg3.jpg'];
 const kleine_plaatjes = ['0.jpg', '1.jpg', '2.jpg', '3.jpg'];
 
-// function knop1() {
-//     groot_plaatje.src = "images/bg1.jpg"
-//     plaatje.src = "images/1.jpg"
-// }
+function knop1() {
+    groot_plaatje.src = "images/bg1.jpg"
+    plaatje.src = "images/1.jpg"
+}
 
-// function knop2() {
-//     groot_plaatje.src = "images/bg2.jpg"
-//     plaatje.src = "images/2.jpg"
-// }
+function knop2() {
+    groot_plaatje.src = "images/bg2.jpg"
+    plaatje.src = "images/2.jpg"
+}
 
-// function knop3() {
-//     groot_plaatje.src = "images/bg3.jpg"
-//     plaatje.src = "images/3.jpg"
-// }
-
-function maken_button() {
-    plaatje = document.getElementById("groot_plaatje");
-    plaatje.src = "images/" + grote_plaatjes[1];
-    plaatje = document.getElementById("plaatje");
-    plaatje.src = "images/" + kleine_plaatjes[1];
+function knop3() {
+    groot_plaatje.src = "images/bg3.jpg"
+    plaatje.src = "images/3.jpg"
 }
