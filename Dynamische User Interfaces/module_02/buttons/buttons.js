6e071352a740925ebaf861283b33efb9e4b6a9f3
Seantheman01 <sean.v.g@outlook.com>
2023-07-03 14:12:06 +0200

    committed

--- Dynamische User Interfaces/module_02/buttons/buttons.js
+++ Dynamische User Interfaces/module_02/buttons/buttons.js
@@ -15,3 +15,10 @@ function knop3() {
     groot_plaatje.src = "images/bg3.jpg"
     plaatje.src = "images/3.jpg"
 }
+
+// function maken_button() {
+//     plaatje = document.getElementById("groot_plaatje");
+//     plaatje.src = "images/" + grote_plaatjes[1];
+//     plaatje = document.getElementById("klein_plaatje");
+//     plaatje.src = "images/" + kleine_plaatjes[1];
+// }
