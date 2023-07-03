61a2847d410e8351f7ba58a7e7ce498f027e3b49
Seantheman01 <sean.v.g@outlook.com>
2023-07-03 15:39:02 +0200

    committed

--- Dynamische User Interfaces/module_02/buttons/buttons.js
+++ Dynamische User Interfaces/module_02/buttons/buttons.js
@@ -4,19 +4,16 @@ const kleine_plaatjes = ['0.jpg', '1.jpg', '2.jpg', '3.jpg'];
 function knop1() {
     groot_plaatje.src = "images/bg1.jpg"
     klein_plaatje.src = "images/1.jpg"
-    document.getElementById("kleur1").style.backgroundColor = "red";
 }
 
 function knop2() {
     groot_plaatje.src = "images/bg2.jpg"
     klein_plaatje.src = "images/2.jpg"
-    document.getElementById("kleur2").style.backgroundColor = "green";
 }
 
 function knop3() {
     groot_plaatje.src = "images/bg3.jpg"
     klein_plaatje.src = "images/3.jpg"
-    document.getElementById("kleur3").style.backgroundColor = "green";
 }
 
 // function maken_button() {
