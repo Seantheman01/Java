3f5d8b4a086c161544216150f0213a1f86d01fa4
Seantheman01 <sean.v.g@outlook.com>
2023-07-05 17:59:47 +0200

    committed

--- Dynamische User Interfaces/module_02/buttons/buttons.js
+++ Dynamische User Interfaces/module_02/buttons/buttons.js
@@ -1,29 +1,27 @@
 const grote_plaatjes = ['bg0.jpg', 'bg1.jpg', 'bg2.jp', 'bg3.jpg'];
 const kleine_plaatjes = ['0.jpg', '1.jpg', '2.jpg', '3.jpg'];
+let getal = 0
 
-function knop1() {
-    groot_plaatje.src = "images/bg1.jpg"
-    klein_plaatje.src = "images/1.jpg"
-    document.getElementById("kleur1").style.backgroundColor = "red";
-}
-
-function knop2() {
-    groot_plaatje.src = "images/bg2.jpg"
-    klein_plaatje.src = "images/2.jpg"
-    document.getElementById("kleur2").style.backgroundColor = "green";
-}
-
-function knop3() {
-    groot_plaatje.src = "images/bg3.jpg"
-    klein_plaatje.src = "images/3.jpg"
-    document.getElementById("kleur3").style.backgroundColor = "green";
-}
-
-// function maken_button(e) {
-//     for(let i = 0; i < e; i++){
-//         plaatje = document.getElementById("groot_plaatje");
-//         plaatje.src = "images/" + grote_plaatjes[0];
-//         plaatje = document.getElementById("klein_plaatje");
-//         plaatje.src = "images/" + kleine_plaatjes[0];
-//     }
+// function knop1() {
+//     groot_plaatje.src = "images/bg1.jpg"
+//     klein_plaatje.src = "images/1.jpg"
 // }
+
+// function knop2() {
+//     groot_plaatje.src = "images/bg2.jpg"
+//     klein_plaatje.src = "images/2.jpg"
+// }
+
+// function knop3() {
+//     groot_plaatje.src = "images/bg3.jpg"
+//     klein_plaatje.src = "images/3.jpg"
+// }
+
+function maken_button(e) {
+    for(let i = 0; i < e; i++){
+        plaatje = document.getElementById("groot_plaatje");
+        plaatje.src = "images/" + grote_plaatjes[1];
+        plaatje = document.getElementById("klein_plaatje");
+        plaatje.src = "images/" + kleine_plaatjes[1];
+    }
+}
